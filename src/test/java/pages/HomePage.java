afdddb15d2193188df2ffc28db78eb19d3776855
Iuliana Dar <yulianadar@gmail.com>
2022-12-25 15:37:04 +0200

    locators and click methods are added in HomePage

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,10 +1,67 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends BasePage {
+
+    @FindBy(xpath = "//h5[contains(text(),'Elements')]")
+    private WebElement elementsMenu;
+
+    @FindBy(xpath = "//h5[contains(text(),'Forms')]")
+    private WebElement formsMenu;
+
+    @FindBy(xpath = "//h5[contains(text(),'Alerts, Frame & Windows')]")
+    private WebElement alertsMenu;
+
+    @FindBy(xpath = "//h5[contains(text(),'Widgets')]")
+    private WebElement widgetsMenu;
+
+    @FindBy(xpath = "//h5[contains(text(),'Interactions')]")
+    private WebElement interactionsMenu;
+
+    @FindBy(xpath = "//h5[contains(text(),'Book Store Application')]")
+    private WebElement bookStoreMenu;
 
     public HomePage(WebDriver driver) {
         super(driver);
     }
+
+   public ElementsPage clickElementsMenu() {
+        click(elementsMenu);
+
+        return new ElementsPage(getDriver());
+   }
+
+   public FormsPage clickFormsMenu() {
+        click(formsMenu);
+
+        return new FormsPage(getDriver());
+   }
+
+   public AlertsFramesWindowsPage clickAlertsFramesWindowsMenu() {
+        click(alertsMenu);
+
+        return new AlertsFramesWindowsPage(getDriver());
+   }
+
+   public WidgetsPage clickWidgetsMenu() {
+        click(widgetsMenu);
+
+        return new WidgetsPage(getDriver());
+   }
+
+   public InteractionsPage clickInteractionsMenu() {
+        click(interactionsMenu);
+
+        return new InteractionsPage(getDriver());
+   }
+
+   public BookStoreApplicationPage clickBookStoreApplicationMenu() {
+        click(bookStoreMenu);
+
+        return new BookStoreApplicationPage(getDriver());
+   }
+
 }
