abff017aee9beb871b9a4571749bbab8d1e4722b
Yurii Ormson <seraph2401@gmail.com>
2022-12-26 18:47:10 +0200

    DF-14 testElementsList added

--- src/test/java/tests/ElementsTest.java
+++ src/test/java/tests/ElementsTest.java
@@ -3,6 +3,10 @@ package tests;
 import base.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.ElementsPage;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class ElementsTest extends BaseTest {
 
@@ -15,6 +19,21 @@ public class ElementsTest extends BaseTest {
                 .getCurrentURL();
 
         Assert.assertEquals(actualURL, expectedURL);
+    }
+    @Test
+    public void testElementsList() {
+        final List<String> expectedSubMenuHeaders = Arrays.asList(
+                "Text Box", "Check Box", "Radio Button", "Web Tables", "Buttons", "Links", "Broken Links - Images",
+                "Upload and Download", "Dynamic Properties"
+        );
+        final int expectedSubMenus = 9;
+
+        ElementsPage elementsPage = openBaseURL().clickElementsMenu();
+
+        elementsPage.checkAllElementsIsVisibleAndClickable();
+
+        Assert.assertEquals(elementsPage.getElementsSubMenuHeaders(), expectedSubMenuHeaders);
+        Assert.assertEquals(elementsPage.getElementsSubMenuAmount(), expectedSubMenus);
     }
 }
 
