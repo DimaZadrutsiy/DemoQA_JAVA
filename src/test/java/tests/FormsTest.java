3d41873b3a862938cc743a0915c75b4cee67e1ab
maksymiasko <maksymiasko@gmail.com>
2022-12-26 15:42:25 +0000

    added testNavigationToFormPage

--- src/test/java/tests/FormsTest.java
+++ src/test/java/tests/FormsTest.java
@@ -1,6 +1,24 @@
 package tests;
 
 import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.FormsPage;
 
 public class FormsTest extends BaseTest {
+
+    @Test
+    public void testNavigationToFormPage() {
+        String expectedUrl = "https://demoqa.com/forms";
+        String expectedTitle = "ToolsQA";
+
+        FormsPage formsPage = openBaseURL().clickFormsMenu();
+
+        String actualUrl = formsPage.getCurrentURL();
+        String actualTitle = formsPage.getTitle();
+
+        Assert.assertNotEquals(actualUrl, BASE_URL);
+        Assert.assertEquals(actualUrl, expectedUrl);
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
 }
