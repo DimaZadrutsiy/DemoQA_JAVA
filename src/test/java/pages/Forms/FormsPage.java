cdcd69d6e2f7780750ac2aa3095cb0ec15d43a95
Max Iasko <115347581+maksymiasko@users.noreply.github.com>
2022-12-27 22:47:03 +0000

    added testNavigationToFormPage (#13)
    added testNavigationToPracticeFormPage
    added testStudentRegistrationPracticeFormPage

--- src/test/java/pages/Forms/FormsPage.java
+++ src/test/java/pages/Forms/FormsPage.java
@@ -1,11 +1,31 @@
 package pages.Forms;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import pages.BasePage;
 
 public class FormsPage extends BasePage {
 
+    @FindBy(xpath = "//div[@class='element-list collapse show']")
+    private WebElement practiceForm;
+
+    @FindBy(xpath = "//div[@class='main-header']")
+    private WebElement h2LogoHeader;
+
     public FormsPage(WebDriver driver) {
         super(driver);
+    }
+
+    public PracticeFormPage clickPracticeForm() {
+        scrollByVisibleElement(practiceForm);
+        click(practiceForm);
+
+        return new PracticeFormPage(getDriver());
+    }
+
+    public String getH2LogoHeaderText() {
+
+        return getText(h2LogoHeader);
     }
 }
