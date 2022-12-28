3063848b1bbcb4a1838d79a6a9ce4df35cf356ad
Dima Zadrutski <dzadruckiy@gmail.com>
2022-12-28 15:06:56 +0200

    testCheckingMessagesWhenButtonsArePressed

--- src/test/java/pages/Elements/ButtonsPage.java
+++ src/test/java/pages/Elements/ButtonsPage.java
@@ -1,10 +1,69 @@
 package pages.Elements;
 
+import com.beust.ah.A;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
 
 public class ButtonsPage extends ElementsPage{
 
+    @FindBy (xpath = "//button[@id='doubleClickBtn']")
+    WebElement button_DoubleClickMe;
+
+    @FindBy (xpath = "//button[@id='rightClickBtn']")
+    WebElement button_RightClickMe;
+
+    @FindBy (xpath = "//button[text()='Click Me']")
+    WebElement button_ClickMe;
+
+    @FindBy (id = "doubleClickMessage")
+    WebElement messageForButton_DoubleClickMe;
+
+    @FindBy (id = "rightClickMessage")
+    WebElement messageForButton_RightClickMe;
+
+    @FindBy (id = "dynamicClickMessage")
+    WebElement messageForButton_ClickMe;
+
+
     public ButtonsPage(WebDriver driver) {
         super(driver);
+    }
+
+    public ButtonsPage clickButton_DoubleClickMe() {
+        Actions actions = new Actions(getDriver());
+        actions.doubleClick(button_DoubleClickMe).perform();
+
+        return this;
+    }
+
+    public ButtonsPage clickButton_RightClickMe() {
+        Actions actions = new Actions(getDriver());
+        actions.contextClick(button_RightClickMe).perform();
+
+        return this;
+    }
+
+    public ButtonsPage clickButton_ClickMe() {
+        Actions actions = new Actions(getDriver());
+        actions.click(button_ClickMe).perform();
+
+        return this;
+    }
+
+    public String messageForButton_DoubleClickMe() {
+
+        return getText(messageForButton_DoubleClickMe);
+    }
+
+    public String messageForButton_RightClickMe() {
+
+        return getText(messageForButton_RightClickMe);
+    }
+
+    public String messageForButton_ClickMe() {
+
+        return getText(messageForButton_ClickMe);
     }
 }
--- src/test/java/pages/Elements/ElementsPage.java
+++ src/test/java/pages/Elements/ElementsPage.java
@@ -12,6 +12,9 @@ public class ElementsPage extends BasePage {
     @FindBy(xpath = "//div[@class = 'element-list collapse show']//span")
     private List<WebElement> elementsList;
 
+    @FindBy(xpath = "//div[@class='element-list collapse show']/ul[@class='menu-list']/li[5]")
+    WebElement Buttons;
+
     public ElementsPage(WebDriver driver) {
         super(driver);
     }
@@ -25,5 +28,12 @@ public class ElementsPage extends BasePage {
     }
     public void checkAllElementsIsVisibleAndClickable() {
         clickAllElementsInList(elementsList);
+    }
+
+    public ButtonsPage clickButtons() {
+        scrollByVisibleElement(Buttons);
+        click(Buttons);
+
+        return new ButtonsPage(getDriver());
     }
 }
