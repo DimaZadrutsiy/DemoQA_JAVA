e5f63a37cb93854a1964fddaa96937ea79280d69
Yurii Ormson <seraph2401@gmail.com>
2023-01-03 22:12:34 +0200

    testOneElementIsUnchecked added

--- src/test/java/tests/ElementsTests/CheckBoxTest.java
+++ src/test/java/tests/ElementsTests/CheckBoxTest.java
@@ -3,7 +3,11 @@ package tests.ElementsTests;
 import base.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.Elements.CheckBoxPage;
 import pages.Elements.ElementsPage;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 public class CheckBoxTest extends BaseTest {
@@ -17,5 +21,27 @@ public class CheckBoxTest extends BaseTest {
                 .clickExpandAll();
 
         Assert.assertTrue(checkBoxPage.openCheckBoxPage().isAllCheckBoxSelected());
+    }
+
+    @Test
+    public void testOneElementIsUnchecked() {
+        final List<String> expectedSelectedCheckboxes = Arrays.asList(
+                "desktop", "notes", "commands",
+                "documents", "workspace", "react", "angular", "veu", "office",
+                "public", "private", "classified", "general", "wordFile"
+        );
+
+        List<String> actualSelectedCheckboxes = openBaseURL()
+                .clickElementsMenu()
+                .openCheckBoxPage()
+                .clickOnHomeCheckBox()
+                .clickExpandAll()
+                .clickOnExcelFileCheckBox()
+                .selectedResult();
+
+        CheckBoxPage checkBoxPage = new CheckBoxPage(getDriver());
+
+        Assert.assertFalse(checkBoxPage.isExcelFileCheckBoxSelected());
+        Assert.assertEquals(actualSelectedCheckboxes,expectedSelectedCheckboxes);
     }
 }
