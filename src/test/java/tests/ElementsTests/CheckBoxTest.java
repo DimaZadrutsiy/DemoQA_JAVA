6a2c1f922f0e5d2c49d81b23799f5219de2a0953
Yurii Ormson <seraph2401@gmail.com>
2023-01-07 16:40:03 +0200

    minor fix added

--- src/test/java/tests/ElementsTests/CheckBoxTest.java
+++ src/test/java/tests/ElementsTests/CheckBoxTest.java
@@ -2,6 +2,7 @@ package tests.ElementsTests;
 
 import base.BaseTest;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import pages.Elements.ElementsPage;
 
