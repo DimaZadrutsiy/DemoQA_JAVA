4028b366295b9ac3493de551511ec4f168460724
Dima Zadrutski <dzadruckiy@gmail.com>
2023-01-08 19:41:07 +0200

    testCheckingNamesButtons testCheckingQuantityButtons testCheckingMainHeader

--- src/test/java/pages/Elements/ButtonsPage.java
+++ src/test/java/pages/Elements/ButtonsPage.java
@@ -5,25 +5,33 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.List;
+
 public class ButtonsPage extends ElementsPage{
 
     @FindBy (xpath = "//button[@id='doubleClickBtn']")
-    WebElement button_DoubleClickMe;
+    private WebElement button_DoubleClickMe;
 
     @FindBy (xpath = "//button[@id='rightClickBtn']")
-    WebElement button_RightClickMe;
+    private WebElement button_RightClickMe;
 
-    @FindBy (xpath = "//button[text()='Click Me']")
-    WebElement button_ClickMe;
+    @FindBy (xpath = "/html/body/div[2]/div/div/div[2]/div[2]/div[2]/div[3]/button")
+    private WebElement button_ClickMe;
 
     @FindBy (id = "doubleClickMessage")
-    WebElement messageForButton_DoubleClickMe;
+    private WebElement messageForButton_DoubleClickMe;
 
     @FindBy (id = "rightClickMessage")
-    WebElement messageForButton_RightClickMe;
+    private WebElement messageForButton_RightClickMe;
 
     @FindBy (id = "dynamicClickMessage")
-    WebElement messageForButton_ClickMe;
+    private WebElement messageForButton_ClickMe;
+
+    @FindBy (css = "#app > div > div > div.row > div.col-12.mt-4.col-md-6 > div:nth-child(2) > div")
+    private List<WebElement> allButtons;
+
+    @FindBy (css = ".main-header")
+    private WebElement mainHeader;
 
 
     public ButtonsPage(WebDriver driver) {
@@ -63,5 +71,20 @@ public class ButtonsPage extends ElementsPage{
     public String messageForButton_ClickMe() {
 
         return getText(messageForButton_ClickMe);
+    }
+
+    public List<String> getNamesAllButtons() {
+
+        return getTexts(allButtons);
+    }
+
+    public int getNumberOfButtons() {
+
+        return getTexts(allButtons).size();
+    }
+
+    public String getMainHeader() {
+
+        return getText(mainHeader);
     }
 }
--- src/test/java/tests/ElementsTests/ButtonTest.java
+++ src/test/java/tests/ElementsTests/ButtonTest.java
@@ -2,13 +2,14 @@ package tests.ElementsTests;
 
 import base.BaseTest;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import pages.Elements.ButtonsPage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ButtonTest extends BaseTest {
 
-    @Ignore
     @Test
     public void testCheckingMessagesWhenButtonsArePressed() {
         final String expectedMessage_Button_DoubleClickMe = "You have done a double click";
@@ -34,5 +35,44 @@ public class ButtonTest extends BaseTest {
         Assert.assertEquals(actualMessage_Button_DoubleClickMe, expectedMessage_Button_DoubleClickMe);
         Assert.assertEquals(actualMessage_Button_RightClickMe, expectedMessage_Button_RightClickMe);
         Assert.assertEquals(actualMessage_Button_ClickMe, expectedMessage_Button_ClickMe);
+    }
+
+    @Test
+    public void testCheckingNamesButtons() {
+        final List<String> expectedNamesButtons = new ArrayList<>();
+        expectedNamesButtons.add("Double Click Me");
+        expectedNamesButtons.add("Right Click Me");
+        expectedNamesButtons.add("Click Me");
+
+        List<String> actualNamesButton = openBaseURL()
+                .clickElementsMenu()
+                .clickButtons()
+                .getNamesAllButtons();
+
+        Assert.assertEquals(actualNamesButton, expectedNamesButtons);
+    }
+
+    @Test
+    public void testCheckingQuantityButtons() {
+        final int expectedNumberOfButtons = 3;
+
+        int actualNumberOfButtons = openBaseURL()
+                .clickElementsMenu()
+                .clickButtons()
+                .getNumberOfButtons();
+
+        Assert.assertEquals(actualNumberOfButtons, expectedNumberOfButtons);
+    }
+
+    @Test
+    public void testCheckingMainHeader() {
+        final String expectedHeader = "Buttons";
+
+        String actualHeader = openBaseURL()
+                .clickElementsMenu()
+                .clickButtons()
+                .getMainHeader();
+
+        Assert.assertEquals(actualHeader, expectedHeader);
     }
 }
