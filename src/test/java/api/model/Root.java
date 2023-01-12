5ea914bd4251fa7c9d9f7031b1502bafa872ec43
Dima Zadrutski <dzadruckiy@gmail.com>
2023-01-12 09:56:45 +0200

    test_API_HttpRequestResponse_TitleOfAllBooks

---
+++ src/test/java/api/model/Root.java
@@ -0,0 +1,17 @@
+package api.model;
+
+import java.util.ArrayList;
+
+public class Root{
+
+    public ArrayList<Book> books;
+
+    public ArrayList<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(ArrayList<Book> books) {
+        this.books = books;
+    }
+}
+
