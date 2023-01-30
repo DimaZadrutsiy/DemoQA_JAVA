627b3af89221bccfa8ce2b5f9dd32aa2e9af77b9
Yuliana Dar <yulianadar@gmail.com>
2023-01-30 19:00:54 +0200

    testCheckAlertOneClick is aded

--- src/test/java/tests/AlertsFramesWindowTest/AlertsTest.java
+++ src/test/java/tests/AlertsFramesWindowTest/AlertsTest.java
@@ -1,6 +1,21 @@
 package tests.AlertsFramesWindowTest;
 
 import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class AlertsTest extends BaseTest {
+
+    @Test
+    public void testCheckOneClickAlert() {
+        final String expectedAlertText = "You clicked a button";
+
+        String actualAlertText = openBaseURL()
+                .clickAlertsFramesWindowsMenu()
+                .selectAlertsSubMenu()
+                .clickOnceAlertButton()
+                .getTextFromAlert();
+
+        Assert.assertEquals(actualAlertText, expectedAlertText);
+    }
 }
