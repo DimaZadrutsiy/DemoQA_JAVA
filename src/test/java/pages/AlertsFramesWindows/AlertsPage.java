53f5969b022fc1c3ae46cc5de6828007b84ec6dd
Yuliana Dar <108284339+JuliaUA@users.noreply.github.com>
2023-01-31 14:55:35 +0200

    DF93 testCheckOneClickAndWaitAlertText is aded (#69)

--- src/test/java/pages/AlertsFramesWindows/AlertsPage.java
+++ src/test/java/pages/AlertsFramesWindows/AlertsPage.java
@@ -3,11 +3,15 @@ package pages.AlertsFramesWindows;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class AlertsPage extends AlertsFramesWindowsPage{
 
     @FindBy(xpath = "//button[@id='alertButton']")
     WebElement oneClickAlertButton;
+
+    @FindBy(xpath = "//button[@id='timerAlertButton']")
+    WebElement oneClickAndWaitAlertButton;
 
     public AlertsPage(WebDriver driver) {
         super(driver);
@@ -16,6 +20,15 @@ public class AlertsPage extends AlertsFramesWindowsPage{
     public AlertsPage clickOnceAlertButton() {
         if (oneClickAlertButton.isDisplayed() && oneClickAlertButton.isEnabled()) {
             oneClickAlertButton.click();
+        }
+
+        return new AlertsPage(getDriver());
+    }
+
+    public AlertsPage clickOnceAndWaitAlertButton() {
+        if (oneClickAlertButton.isDisplayed() && oneClickAlertButton.isEnabled()) {
+            oneClickAndWaitAlertButton.click();
+            getWait20().until(ExpectedConditions.alertIsPresent());
         }
 
         return new AlertsPage(getDriver());
