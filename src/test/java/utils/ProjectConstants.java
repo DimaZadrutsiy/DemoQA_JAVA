41b6443b6ea0dde5acb42a1e103cfe1976cc2b89
maksymiasko <maksymiasko@gmail.com>
2023-02-01 17:02:08 +0000

    Added ProjectConstants class Refactoring classes, variables

---
+++ src/test/java/utils/ProjectConstants.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class ProjectConstants {
+    public static final String BASE_URL = "https://demoqa.com/";
+    public static final String TITLE = "DEMOQA";
+    public static final String DROPPABLE_HEADER = "Droppable";
+    public static final String DROPPABLE_URL = BASE_URL + "droppable";
+    public static final String SELECTABLE_HEADER = "Selectable";
+    public static final String SELECTABLE_URL = BASE_URL + "selectable";
+    public static final String FORMS_HEADER = "Forms";
+    public static final String FORMS_URL = BASE_URL + "forms";
+    public static final String PRACTICE_FORMS_HEADER = "Practice Form";
+    public static final String PRACTICE_FORMS_URL = BASE_URL + "automation-practice-form";
+}
