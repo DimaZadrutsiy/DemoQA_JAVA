a863e5b80bea353a40fa1bccfdbe289927155136
Yuliana Dar <108284339+JuliaUA@users.noreply.github.com>
2023-02-26 08:27:27 +0200

    testCheckLargeModalBoxText is added (#95)

--- src/test/java/tests/AlertsFramesWindowTest/ModalDialogsTest.java
+++ src/test/java/tests/AlertsFramesWindowTest/ModalDialogsTest.java
@@ -18,4 +18,24 @@ public class ModalDialogsTest extends BaseTest {
 
         Assert.assertEquals(actualText, expectedText);
     }
+
+    @Test
+    public void testCheckLargeModalDialogBoxText() {
+        final String expectedText = "Lorem Ipsum is simply dummy text of the printing and" +
+                " typesetting industry. Lorem Ipsum has been the industry's standard dummy text " +
+                "ever since the 1500s, when an unknown printer took a galley of type and scrambled " +
+                "it to make a type specimen book. It has survived not only five centuries, " +
+                "but also the leap into electronic typesetting, remaining essentially unchanged. " +
+                "It was popularised in the 1960s with the release of Letraset sheets containing Lorem " +
+                "Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker" +
+                " including versions of Lorem Ipsum.";
+
+        String actualText = openBaseURL()
+                .clickAlertsFramesWindowsMenu()
+                .selectModalDialogSubMenu()
+                .clickLargeModalButton()
+                .getLargeModalBoxText();
+
+        Assert.assertEquals(actualText, expectedText);
+    }
 }
