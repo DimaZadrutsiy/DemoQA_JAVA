a49d619782eb6407a95b7e896b28400ee254df9d
Yurii Ormson <seraph2401@gmail.com>
2023-03-06 16:39:45 +0200

    added retryAnalyzer to testAllCheckBoxSelected

--- src/test/java/tests/ElementsTests/CheckBoxTest.java
+++ src/test/java/tests/ElementsTests/CheckBoxTest.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 public class CheckBoxTest extends BaseTest {
 
-    @Test
+    @Test(retryAnalyzer = Retry.class)
     public void testAllCheckBoxSelected() {
         CheckBoxPage checkBoxPage = openBaseURL()
                 .clickElementsMenu()
