60c1ed258bd6eae37882367073d288922b38fa46
JuliaUA <yulianadar@gmail.com>
2023-03-08 21:30:58 +0200

    testCloseSmallodalDialogBox and testLargeModalDialogBox are added

--- src/test/java/pages/AlertsFramesWindows/ModalDialogsPage.java
+++ src/test/java/pages/AlertsFramesWindows/ModalDialogsPage.java
@@ -19,12 +19,18 @@ public class ModalDialogsPage extends AlertsFramesWindowsPage {
     @FindBy(xpath = "//div[@class='modal-body']")
     WebElement largeModalDialogBody;
 
+    @FindBy(xpath = "//button[@id='closeSmallModal']")
+    WebElement closeSmallModalDialogButton;
+
+    @FindBy(xpath = "//button[@id='closeLargeModal']")
+    WebElement closeLargeModalDialogButton;
+
     public ModalDialogsPage(WebDriver driver) {
         super(driver);
     }
 
     public ModalDialogsPage clickSmallModalButton() {
-        if(smallModalButton.isEnabled() && smallModalButton.isDisplayed()) {
+        if (smallModalButton.isEnabled() && smallModalButton.isDisplayed()) {
             click(smallModalButton);
 
             return new ModalDialogsPage(getDriver());
@@ -40,7 +46,7 @@ public class ModalDialogsPage extends AlertsFramesWindowsPage {
     }
 
     public ModalDialogsPage clickLargeModalButton() {
-        if(largeModalButton.isEnabled() && largeModalButton.isDisplayed()) {
+        if (largeModalButton.isEnabled() && largeModalButton.isDisplayed()) {
             click(largeModalButton);
 
             return new ModalDialogsPage(getDriver());
@@ -53,5 +59,53 @@ public class ModalDialogsPage extends AlertsFramesWindowsPage {
         getWait20().until(ExpectedConditions.visibilityOf(largeModalDialogBody));
 
         return largeModalDialogBody.getText();
+    }
+
+    public ModalDialogsPage clickCloseSmallModalDialogButton() {
+        getWait10().until(ExpectedConditions.visibilityOf(closeSmallModalDialogButton));
+        closeSmallModalDialogButton.click();
+        getWait10().until(ExpectedConditions.invisibilityOf(closeSmallModalDialogButton));
+
+        return new ModalDialogsPage(getDriver());
+    }
+
+    public ModalDialogsPage clickCloseLargeModalDialogButton() {
+        getWait10().until(ExpectedConditions.visibilityOf(closeLargeModalDialogButton));
+        closeLargeModalDialogButton.click();
+        getWait10().until(ExpectedConditions.invisibilityOf(closeLargeModalDialogButton));
+
+        return new ModalDialogsPage(getDriver());
+    }
+
+    public boolean isLargeModalDialogBoxDisplayed() {
+
+        try {
+            if (closeLargeModalDialogButton.isDisplayed() && largeModalDialogBody.isDisplayed()) {
+
+                return true;
+            } else {
+
+                return false;
+            }
+        } catch (Exception e) {
+
+            return false;
+        }
+    }
+
+    public boolean isSmallModalDialogBoxDisplayed() {
+
+        try {
+            if (closeSmallModalDialogButton.isDisplayed() && smallModalDialogBody.isDisplayed()) {
+
+                return true;
+            } else {
+
+                return false;
+            }
+        } catch (Exception e) {
+
+            return false;
+        }
     }
 }
